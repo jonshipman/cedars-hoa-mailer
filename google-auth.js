4f043b5d2fe975ab1b271afdfd9020fb05607ca8
Jon Shipman <jon@jonshipman.com>
2023-02-09 20:38:14 -0600

    sheet id as prop

--- google-auth.js
+++ google-auth.js
@@ -2,8 +2,8 @@ import fs from 'node:fs/promises';
 
 import { GoogleSpreadsheet } from 'google-spreadsheet';
 
-export async function load() {
-	const doc = new GoogleSpreadsheet(process.env.SHEETID);
+export async function load(id = process.env.SHEETID) {
+	const doc = new GoogleSpreadsheet(id);
 
 	const file = await fs.readFile('auth.json');
 	const creds = JSON.parse(file);
